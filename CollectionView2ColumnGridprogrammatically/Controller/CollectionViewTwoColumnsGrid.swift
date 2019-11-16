fc012d195a18cd874b5c3092862351888bc9470e
Hameed abdullah <hameed.2009@yahoo.com>
2019-11-15 20:25:35 -0500

    setup CollectionView

--- CollectionView2ColumnGridprogrammatically/Controller/CollectionViewTwoColumnsGrid.swift
+++ CollectionView2ColumnGridprogrammatically/Controller/CollectionViewTwoColumnsGrid.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class CollectionViewTwoColumnsGrid: UICollectionViewController {
+class CollectionViewTwoColumnsGrid: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
+    fileprivate let cellId = "cellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.backgroundColor = .white
-        navigationItem.title = "Two Columns Grid"
+        setupCollectionView()
         
     }
     
     
+    fileprivate func setupCollectionView() {
+        collectionView.backgroundColor = .white
+        navigationItem.title = "Two Columns Grid"
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 200)
+    }
     
     //we need this init when creating a collectionView Programmatically otherwise app will crash
     init() {
